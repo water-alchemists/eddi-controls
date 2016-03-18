e83ffe75bef297df87fe37c1894676d6d6603fc3
Tony <terimyaki@users.noreply.github.com>
2016-03-17 20:57:08 -0400

    added set up for event listeners

---
+++ eddi-fire.js
@@ -0,0 +1,124 @@
+'use strict';
+const firebase = require('firebase');
+
+const PATHS = {
+	BASE : 'https://eddi.firebaseIO.com',
+	EDDI : 'eddis',
+	SETTINGS : 'settings',
+	STATE : 'state',
+	TIMING : 'timing',
+	START : 'start',
+	END : 'end',
+	HOUR : 'hour',
+	MINUTE : 'minute'
+};
+
+const EVENTS = {
+	start : 'start',
+	end : 'end', 
+	state : 'state'
+};
+
+const EDDI_ID = process.env.EDDI_ID;
+
+class EddiFire {
+	constructor(){
+		const ref = new Firebase(PATHS.BASE_PATH);
+
+		this.EDDI = ref.child(PATHS.EDDI).child(EDDI_ID);
+
+		this.subscribers = {
+			[EVENTS.start] : [],
+			[EVENTS.end] : [],
+			[EVENTS.state] : []
+		};
+
+		//these will be updated as values come in from firebase
+		this.state = null;
+		this.start = null;
+		this.end = null;
+	}
+
+	init(){
+		//alert all functions listening to the change of the state
+		this.EDDI
+			.child(PATHS.SETTINGS)
+			.child(PATHS.STATE)
+			.on('value', snapshot => {
+				//get and parse value
+				const data = snapshot.val();
+
+				//updates the value from firebase for referencing later
+				this.state = data;
+
+				//lets the subscribers know
+				this.subscribers[EVENTS.state].forEach(func => func(data));
+			});
+
+		//alert all functions listening to the change of the start time
+		this.EDDI
+			.child(PATHS.SETTINGS)
+			.child(PATHS.START)
+			.on('value', snapshot => {
+				//get and parse value
+				var data = snapshot.val();
+				if(data) {
+					const hrs = data[PATHS.HOUR],
+						mins = data[PATHS.MINUTE];
+					data = new Date().setHours(hrs).setMinutes(mins);
+				}
+
+				//updates the value from firebase for referencing later
+				this.start = data;
+
+				//lets the subscribers know
+				this.subscribers[EVENTS.start].forEach(func => func(data));
+			});
+
+		//alert all functions listening to the change of the end time
+		this.EDDI
+			.child(PATHS.SETTINGS)
+			.child(PATHS.END)
+			.on('value', snapshot => {
+				//get and parse value
+				var data = snapshot.val();
+				if(data) {
+					const hrs = data[PATHS.HOUR],
+						mins = data[PATHS.MINUTE];
+					data = new Date().setHours(hrs).setMinutes(mins);
+				}
+
+				//updates the value from firebase for referencing later
+				this.end = data;
+
+				//lets the subscribers know
+				this.subscribers[EVENTS.end].forEach(func => func(data));
+			});
+
+	}
+
+	register(event, func){
+		//registers listeners
+		const type = EVENTS[event];
+		if(!type) throw new Error(`Not a valid event type. These are valid : ${Object.keys(EVENTS).reduce((accum, key) => `${accum} "${key}"`, '')}`);
+		this.subscribers[type].push(func);
+	}
+
+	unregister(event, func){
+		//unregisters listeners
+		const type = EVENTS[event];
+		if(!type) throw new Error(`Not a valid event type. These are valid : ${Object.keys(EVENTS).reduce((accum, key) => `${accum} "${key}"`, '')}`);
+		const index = this.subscribers[type].indexOf(func);
+		if(index > -1) this.subscribers[type].splice(index, 1);
+	}
+
+}
+
+module.exports = function(){
+	var init;
+	if(init) return init;
+	else {
+		init = new EddiFire();
+		return init;
+	}
+}
