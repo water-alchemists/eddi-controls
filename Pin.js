ea0dae2dd82635f993b9142709116e9916186a26
Tony <terimyaki@users.noreply.github.com>
2016-03-22 22:23:42 -0400

    uncomment

--- Pin.js
+++ Pin.js
@@ -23,28 +23,28 @@ var Pin = function(pin){
 
 Pin.prototype.initialize = function(){
   // THE REAL DEAL
-  // return new Promise( (resolve, reject) => {
-  //   fs.writeFile("/sys/class/gpio/export", this.num+"\n", (err) => {
-  //     if( err ){
-  //       console.error(err);
-  //       process.exit(1);
-  //       return reject();
-  //     }
-  //     fs.writeFile("/sys/class/gpio/gpio"+this.num+"/direction", "out\n", (err) => {
-  //       if( err ){
-  //         console.error(err);
-  //         process.exit(1);
-  //         return reject();
-  //       }
-  //       this.ready = true;
-  //       return this.off().then(resolve);
-  //     });
-  //   });
-  // });
+  return new Promise( (resolve, reject) => {
+    fs.writeFile("/sys/class/gpio/export", this.num+"\n", (err) => {
+      if( err ){
+        console.error(err);
+        process.exit(1);
+        return reject();
+      }
+      fs.writeFile("/sys/class/gpio/gpio"+this.num+"/direction", "out\n", (err) => {
+        if( err ){
+          console.error(err);
+          process.exit(1);
+          return reject();
+        }
+        this.ready = true;
+        return this.off().then(resolve);
+      });
+    });
+  });
   // FOR TESTING ON LOCAL MACHINE
-  console.log(`${this.pin} initialize with mapping ${this.num}`);
-  this.ready = true
-  return this.off();
+  // console.log(`${this.pin} initialize with mapping ${this.num}`);
+  // this.ready = true
+  // return this.off();
 }
 
 
@@ -52,22 +52,22 @@ Pin.prototype._set = function(val){
   this.state = val;
   console.log("Writing to pin "+this.pin+", value: "+val);
   // THE REAL DEAL
-  // return new Promise((resolve, reject) => {
-  //   if( !this.ready ){
-  //     reject( new Error("You must initialize a Pin before using it") );
-  //   }
-  //   fs.writeFile(this.file, val+"\n", function(err){
-  //     if( err ){
-  //       console.error(err);
-  //       return reject(err);
-  //     }
-  //     resolve();
-  //   });
-  // });
+  return new Promise((resolve, reject) => {
+    if( !this.ready ){
+      reject( new Error("You must initialize a Pin before using it") );
+    }
+    fs.writeFile(this.file, val+"\n", function(err){
+      if( err ){
+        console.error(err);
+        return reject(err);
+      }
+      resolve();
+    });
+  });
 
   // FOR TESTING ON LOCAL MACHINE
-  if(!this.ready) return Promise.reject(new Error("You must initialize a Pin before using it"));
-  return Promise.resolve();
+  // if(!this.ready) return Promise.reject(new Error("You must initialize a Pin before using it"));
+  // return Promise.resolve();
 }
 
 Pin.prototype.isOn = function(){
